a76891a7a499c6c3b511bac2422ef6013f884d5c
Simon Tett <tetts@sci1.jasmin.ac.uk>
2023-11-19 14:37:01 +0000

    Setup env for jasmin.

---
+++ set_env_jasmin.sh
@@ -0,0 +1,5 @@
+# setup env for running rpy2 and xarray on jasmin
+# run before import gev_r or anything else that uses R
+module load jasr # load the jasmin r env.
+conda activate /home/users/tetts/CPMpy # my python + R env
+
