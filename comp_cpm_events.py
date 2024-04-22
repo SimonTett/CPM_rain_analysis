1/2 way through fixing possible bug in event calculation involvin land/sea sep.

comp_cpm_events.py
@@ -18,7 +18,7 @@ cpm_cet = cpm_cet.tas.resample(time='QS-DEC').mean()  # resample to seasonal mea
 cpm_cet_jja = cpm_cet.where(cpm_cet.time.dt.season == 'JJA', drop=True)  # and pull out summer.
 
 # Load up CPM extreme rainfall data, select to region of interest and mask
-rgn_all = dict(longitude=slice(357.5, 361.0), latitude=slice(1.5, 7.5))  # region for which extraction was done.
+
 rgn = CPMlib.carmont_rgn
 filtered = False
 if filtered:
@@ -27,34 +27,20 @@ if filtered:
 else:
     paths = sorted(CPMlib.CPM_dir.glob('CPM*/*.nc'))
     outpath = CPMlib.CPM_dir / "CPM_all_events.nc"
-my_logger.info(f"Opening data for {len(paths)}")
+my_logger.info(f"Opening data for {len(paths)} files")
 ds = xarray.open_mfdataset(paths, parallel=True)
 ds = ds.where(ds.time.dt.season == 'JJA', drop=True)  # get summer only
-ds = ds.sel(**rgn)  # reduce to rgn of interest
-print("Opened data")
-topog = xarray.load_dataset(CPM_rainlib.dataDir / 'orog_land-cpm_BI_2.2km.nc', decode_times=False)
-topog = topog.ht.sel(rgn_all).rename(dict(longitude='grid_longitude', latitude='grid_latitude')).squeeze()
-t = topog.coarsen(grid_longitude=2, grid_latitude=2, boundary='trim').min().sel(rgn)
-tmn = topog.coarsen(grid_longitude=2, grid_latitude=2, boundary='trim').mean().sel(rgn)
-# check rel errors < 1.e-6
-max_rel_lon = float((np.abs(t.grid_longitude.values / ds.grid_longitude.values - 1)).max())
-max_rel_lat = float((np.abs(t.grid_latitude.values / ds.grid_latitude.values - 1)).max())
-
-if max_rel_lon > 1e-6:
-    raise ValueError(f"Lon Grids differ by more than 1e-6. Max = {max_rel_lon}")
-
-if max_rel_lat > 1e-6:
-    raise ValueError(f"Lat Grids differ by more than 1e-6. Max = {max_rel_lat}")
-t = t.interp(grid_longitude=ds.grid_longitude, grid_latitude=ds.grid_latitude)  # grids differ by tiny amount.
-tmn = tmn.interp(grid_longitude=ds.grid_longitude, grid_latitude=ds.grid_latitude)
-
-# iterate over ensemble_member and then stack them at the end...
+ds = ds.sel(**rgn).load()  # reduce to rgn of interest
+CPM_rainlib.fix_coords(ds)
+topog = xarray.load_dataarray(CPM_rainlib.dataDir / 'cpm_topog_fix_c2.nc').sel(**rgn)
+my_logger.info("Loaded data")
+## iterate over ensemble_member and then stack them at the end...
 grped_list = []
 for ensemble in ds.ensemble_member:
     my_logger.info(f"Processing ensemble: {int(ensemble)}")
-    dataset = ds.sel(ensemble_member=ensemble)  # extract the ensemble
+    dataset = ds.sel(ensemble_member=ensemble) # extract the ensemble
     mxTime = dataset.seasonalMaxTime.squeeze(drop=True).load()
-    grp = CPMlib.discretise(mxTime).where(t > 1, 0).rename('EventTime')
+    grp = CPMlib.discretise(mxTime).where(topog > 0, 0).rename('EventTime')
     # remove unwanted co-ords.
     coords_to_drop = [c for c in mxTime.coords if c not in mxTime.dims]
     # add on t & surface.
@@ -77,7 +63,7 @@ for ensemble in ds.ensemble_member:
         dd['CET'] = cet_extreme_times  # add in the CET data.
         dd_lst.append(dd)
         # add in hts
-        ht = tmn.sel(grid_longitude=dd.x, grid_latitude=dd.y)
+        ht = topog.sel(grid_longitude=dd.x, grid_latitude=dd.y)
         # drop unnneded coords
         coords_to_drop = [c for c in ht.coords if c not in ht.dims]
         ht = ht.drop_vars(coords_to_drop)

comp_topog.py
@@ -1,4 +1,4 @@
-# compute topography for different radar datasets
+# compute topography for carmont region different radar datasets and CPM dataset.
 import CPM_rainlib
 import CPMlib
 import xarray
@@ -22,10 +22,8 @@ commonLib.init_log(my_logger,level='INFO')
 out_dir = CPMlib.radar_dir / 'topography'
 out_dir.mkdir(exist_ok=True,parents=True)
 # get in an example field from the radar for each radar type
-rgn = {k:slice(v-150e3,v+150e3) for k,v in CPMlib.carmont_drain_OSGB.items()}
-# fix the rgn a bit!
-rgn['projection_x_coordinate'] = slice(rgn['projection_x_coordinate'].start,
-                                       rgn['projection_x_coordinate'].start+225e3)
+rgn = {k:slice(v-75e3,v+75e3) for k,v in CPMlib.carmont_drain_OSGB.items()}
+
 rgn_extent = []
 for v in rgn.values():
     rgn_extent.extend([v.start,v.stop])
@@ -38,35 +36,59 @@ for path in files:
     radar_ds[name] = radar
 my_logger.info(f"Loaded radar data")
 region = CPMlib.carmont_rgn_OSGB
-topog = CPM_rainlib.read_90m_topog(region=rgn).drop_vars(['band','spatial_ref'])
+topog_cpm = CPM_rainlib.read_90m_topog(region=rgn).drop_vars(['band','spatial_ref'])
 my_logger.info(f"Loaded topography")
 radar_top=dict()
 for key,radar in radar_ds.items():
     delta = dx_dy(radar)
     npt = np.floor(delta/90.).astype(int)
     minp=int(0.5*npt[0]*npt[1])
-    topog_avg = topog.rolling(projection_x_coordinate=npt[0],projection_y_coordinate=npt[1],
-                              center=True,min_periods=minp).mean() # average up to rough resoln of radar
+    topog_avg = topog_cpm.rolling(projection_x_coordinate=npt[0],projection_y_coordinate=npt[1],
+                              center=True,min_periods=minp).mean().sel(**rgn).load() # average up to rough resoln of radar
     radar_top[key] = topog_avg.interp_like(radar) # extract pts at radar grid.
     my_logger.info(f"Computed topography for {key}")
-## save the data
+## save the radar data
 for key,r_topog in radar_top.items():
     outpath = out_dir/f'topog_{key}.nc'
     r_topog.to_netcdf(outpath)
     my_logger.info(f"Saved topography for {key} to {outpath}")
 
+## compute the CPM topography.
+topog_cpm = xarray.load_dataset(CPM_rainlib.dataDir / 'orog_land-cpm_BI_2.2km.nc', decode_times=False).ht.squeeze()
+topog_cpm = topog_cpm.drop_vars(['t', 'surface'], errors='ignore')
+topog_cpm = topog_cpm.rename(dict(longitude='grid_longitude', latitude='grid_latitude'))[1:, 1:]
+topog_cpm = topog_cpm.coarsen(grid_longitude=2, grid_latitude=2, boundary='pad').mean()
+CPM_rainlib.fix_coords(topog_cpm)
+topog_cpm = topog_cpm.sel(**CPMlib.carmont_rgn)
+outpath= CPM_rainlib.dataDir / 'cpm_topog_fix_c2.nc'
+topog_cpm.to_netcdf(outpath)
+my_logger.info(f"Saved topography for CPM to {outpath}") # save it
 ## plot the topographies for inspection
+# CPM first
+levels=[0, 50, 100, 200, 300, 400, 500, 600, 700, 800,1000,1200]
+fig_cpm_topog,ax = plt.subplots(nrows=1,ncols=1,num='CPM Topog',
+                                   figsize=[5,5],clear=True,subplot_kw=dict(projection=CPMlib.projRot))
+ax.set_extent(rgn_extent,crs=CPMlib.projOSGB)
+topog_cpm.plot(ax=ax,add_colorbar=True,cmap='YlOrBr',levels=levels,label='height (m)',cbar_kwargs=CPMlib.kw_colorbar,transform=CPMlib.projRot)
+ax.set_title('CPM')
+CPM_rainlib.std_decorators(ax)
+CPMlib.plot_carmont(ax)
+fig_cpm_topog.show()
+
 nplots = len(radar_top)
 nrows = int(np.floor(np.sqrt(nplots)))
 ncols = int(np.ceil(nplots/nrows))
-fig_topog,axes = plt.subplots(nrows=nrows,ncols=ncols,figsize=[11,7],clear=True,num='radar_topog',layout='constrained',
+fig_topog,axes = plt.subplots(nrows=nrows,ncols=ncols,figsize=[11,7],clear=True,num='Topog',layout='constrained',
                               subplot_kw=dict(projection=CPMlib.projOSGB))
-levels=[0, 50, 100, 200, 300, 400, 500, 600, 700, 800,1000,1200]
+
 for (key,topog),ax in zip(radar_top.items(),axes.flat):
-    ax.set_extent(rgn_extent,crs=CPMlib.projOSGB)
-    cm=topog.plot(ax=ax,add_colorbar=False,cmap='YlOrBr',levels=levels)
+    ax.set_extent(rgn_extent, crs=CPMlib.projOSGB)
+    cm=topog.plot(ax=ax, add_colorbar=False, cmap='YlOrBr', levels=levels)
     ax.set_title(key)
     CPM_rainlib.std_decorators(ax)
+    CPMlib.plot_carmont(ax)
+
+
 fig_topog.colorbar(cm,ax=axes,label='height (m)',**CPMlib.kw_colorbar)
 fig_topog.show()
 

plot_kde_smooth_events.py
@@ -54,10 +54,15 @@ for (q,rolling),axis in zip(itertools.product([0.5],[1,4]),axs):
     radar_c5_quant = radar_dataset_c5.sel(quantv=q,rolling=rolling).rename(EventTime='idx').dropna('idx')
     my_logger.debug(f"Loaded radar")
     for ds,area in zip([radar_quant, radar_c4_quant, radar_c5_quant, quant, raw_quant], [25.0, 16.0, 25, 4.4 * 4.4, 4.4 * 4.4]):
-        ds['log10 area'] = np.log10(ds.count_cells*area)
+        L = (ds.t.dt.season == 'JJA' ) & (ds.t.dt.year >=2008 )  &  (ds.t.dt.year <= 2023)
+        ds['Area'] = ds.count_cells*area
         ds['Hour'] = ds.t.dt.hour
         ds['Accum'] = ds.max_precip*rolling
-    my_logger.debug(f"Computed log10_area, hour, accum")
+        try:
+            ds['indx_2020_08_12'] = ds.idx.where(ds.t.dt.strftime('%Y-%m-%d') == '2020-08-12',drop=True).values[0]
+        except IndexError:
+            pass
+    my_logger.debug(f"Computed area, hour, accum")
     pos = axis[0].get_position()
     y=(pos.ymax+pos.ymin)/2
     x=0.02
@@ -67,9 +72,9 @@ for (q,rolling),axis in zip(itertools.product([0.5],[1,4]),axs):
 
 
     for ax,var,bins,xlabel in zip(axis.flatten(),
-                        ['Hour','log10 area','height','Accum'],
+                        ['Hour','Area','height','Accum'],
                         [24,20,20,20], # bin sizes
-                        ['Hour',r'$\log_{10}$ Area','Height (m)','Accum precip (mm)']     ):
+                        ['Hour','Area (km$^2$)','Height (m)','Accum precip (mm)']     ):
 
         for ds,name in zip([quant, raw_quant, radar_quant, radar_c4_quant, radar_c5_quant],
                                  ['CPM','CPM-Raw','5km','1km-c4','1km-c5']):
@@ -78,6 +83,9 @@ for (q,rolling),axis in zip(itertools.product([0.5],[1,4]),axs):
                color='brown'
             sns.kdeplot(ds[var],ax=ax,label=f'{name} ',
                         color=color,linewidth=2,cut=0)
+            if 'km' in name: # radar data.
+                ax.axvline(ds[var].sel(idx=ds.indx_2020_08_12)+np.random.uniform(low=-0.3,high=0.3),color=color,linestyle='dashed')
+
 
 
 
@@ -85,7 +93,8 @@ for (q,rolling),axis in zip(itertools.product([0.5],[1,4]),axs):
         ax.xaxis.set_major_locator(MaxNLocator(nbins=4))
         labels.plot(ax)
 
-
+for ax in axs:
+    ax[1].set_xscale('log')
 axs[-1][-1].set_xlim(0,50)
 axs[0][1].legend(fontsize='small',loc='lower left')
 fig.show()

print_stat_fits.py
@@ -30,14 +30,12 @@ def comp_land_mn(ds: xarray.Dataset, topog: xarray.DataArray):
     return mn
 
 
-def fix_coords(ds: xarray.Dataset):
-    for c in CPM_rainlib.cpm_horizontal_coords:
-        ds[c] = ds[c].astype(np.float32).round(2)  # round co-ords
+
 
 
 def load_dataset(f: pathlib.Path):
     ds = xarray.load_dataset(f)
-    fix_coords(ds)
+    CPM_rainlib.fix_coords(ds)
     return ds
 
 
@@ -72,12 +70,8 @@ for f in raw_fit_files:
 
 my_logger.info("Read in the fits")
 # msk to land
-topog = xarray.load_dataset(CPM_rainlib.dataDir / 'orog_land-cpm_BI_2.2km.nc', decode_times=False).ht.squeeze()
-topog = topog.drop_vars(['t', 'surface'], errors='ignore')
-topog = topog.rename(dict(longitude='grid_longitude', latitude='grid_latitude'))[1:, 1:]
-topog = topog.coarsen(grid_longitude=2, grid_latitude=2, boundary='pad').mean(). \
-    sel(**CPMlib.carmont_rgn).load()
-fix_coords(topog)
+topog = xarray.load_dataarray(CPM_rainlib.dataDir / 'cpm_topog_fix_c2.nc').sel(**CPMlib.carmont_rgn)
+
 my_logger.info("Read in the topography")
 
 # make dataframes of AIC and KS stats